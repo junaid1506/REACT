5d64c5699106987fec7c92846e2ed0be808d05c5
junaid1506 <junnu2757@gmail.com>
2025-11-03 13:50:40 +0530

    Update

--- 18 Fragments/learning-fragment/src/App.jsx
+++ 18 Fragments/learning-fragment/src/App.jsx
@@ -1,13 +1,23 @@
-import './App.css'
+import "./App.css";
+import React from "react";
 
 function App() {
-
-
+  let foodItems = ['Daal','Green Vegetables','Roti','Salad','Milk']
   return (
-    <>
-    
-    </>
-  )
+    <React.Fragment>
+    <h1>Healthy Food</h1>
+      <ul className="list-group">
+        {
+          foodItems.map(item=>(
+            <li class="list-group-item">{item}</li>
+          ))
+        }
+       
+      </ul>
+  </React.Fragment>
+  );
+
+  // Short hand is <></> of <React.Fragment> </React.Fragment>
 }
 
-export default App
+export default App;
