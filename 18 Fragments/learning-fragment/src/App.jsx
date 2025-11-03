f5cc9de758d28e097cf9e2a166f75d49e4237f75
junaid1506 <junnu2757@gmail.com>
2025-11-03 17:25:39 +0530

    Update

--- 18 Fragments/learning-fragment/src/App.jsx
+++ 18 Fragments/learning-fragment/src/App.jsx
@@ -3,13 +3,15 @@ import React from "react";
 
 function App() {
   let foodItems = ['Daal','Green Vegetables','Roti','Salad','Milk', 'Ghee', 'Fish']
+  // let foodItems = []
   return (
     <React.Fragment>
     <h1>Healthy Food</h1>
+    {foodItems.length === 0 ? <h3>I am still hungery</h3> : null}
       <ul className="list-group">
         {
           foodItems.map(item=>(
-            <li className="list-group-item">{item}</li>
+            <li key={item} className="list-group-item">{item}</li>
           ))
         }
       </ul>
