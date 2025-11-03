6d806fb210425e3880e6e65164344dacfea6b123
junaid1506 <junnu2757@gmail.com>
2025-11-03 17:01:37 +0530

    Update

--- 18 Fragments/learning-fragment/src/App.jsx
+++ 18 Fragments/learning-fragment/src/App.jsx
@@ -2,17 +2,16 @@ import "./App.css";
 import React from "react";
 
 function App() {
-  let foodItems = ['Daal','Green Vegetables','Roti','Salad','Milk', 'Ghee']
+  let foodItems = ['Daal','Green Vegetables','Roti','Salad','Milk', 'Ghee', 'Fish']
   return (
     <React.Fragment>
     <h1>Healthy Food</h1>
       <ul className="list-group">
         {
           foodItems.map(item=>(
-            <li class="list-group-item">{item}</li>
+            <li className="list-group-item">{item}</li>
           ))
         }
-       
       </ul>
   </React.Fragment>
   );
