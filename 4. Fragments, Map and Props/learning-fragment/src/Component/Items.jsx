d25a96981f0428bb4f3c5533efff951caa2668d0
junaid1506 <junnu2757@gmail.com>
2025-11-06 13:01:09 +0530

    Update

--- 4. Fragments, Map and Props/learning-fragment/src/Component/Items.jsx
+++ 4. Fragments, Map and Props/learning-fragment/src/Component/Items.jsx
@@ -1,8 +1,14 @@
-const Items = ({singleItem}) => {
+const Items = ({ singleItem }) => {
+  const clickedHandle = () => {
+    alert(`${singleItem} is add to the beg.`);
+  };
+
   return (
     <li className="list-group-item  flex">
       {singleItem}
-      <button type="button" class="btn btn-info">Add </button>
+      <button type="button" className="btn btn-info" onClick={clickedHandle}>
+        Add +{" "}
+      </button>
     </li>
   );
 };
