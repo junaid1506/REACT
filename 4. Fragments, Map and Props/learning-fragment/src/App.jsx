ff5efc1b6490a9786b0a0547171baf4cb79ff592
junaid1506 <junnu2757@gmail.com>
2025-11-07 17:52:26 +0530

    Update

--- 4. Fragments, Map and Props/learning-fragment/src/App.jsx
+++ 4. Fragments, Map and Props/learning-fragment/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import ErrorMsg from "./Component/ErrorMsg";
 import AllItems from "./Component/AllItems";
 import Container from "./Component/Container";
@@ -15,122 +15,127 @@ function App() {
     "Ghee",
     "Fish",
   ];
-//   let foodItems2 = [
-//   // 🥣 Breakfast Items
-//   "Poha",
-//   "Upma",
-//   "Idli",
-//   "Dosa",
-//   "Paratha",
-//   "Omelette",
-//   "Boiled Eggs",
-//   "Bread Butter",
-//   "Cornflakes",
-//   "Pancakes",
-//   "Tea",
-//   "Coffee",
-//   "Milk",
-//   "Green Tea",
-//   "Fruit Juice",
+  //   let foodItems2 = [
+  //   // 🥣 Breakfast Items
+  //   "Poha",
+  //   "Upma",
+  //   "Idli",
+  //   "Dosa",
+  //   "Paratha",
+  //   "Omelette",
+  //   "Boiled Eggs",
+  //   "Bread Butter",
+  //   "Cornflakes",
+  //   "Pancakes",
+  //   "Tea",
+  //   "Coffee",
+  //   "Milk",
+  //   "Green Tea",
+  //   "Fruit Juice",
 
-//   // 🍛 Lunch/Dinner Items
-//   "Rice",
-//   "Pulao",
-//   "Biryani",
-//   "Khichdi",
-//   "Dal Tadka",
-//   "Rajma",
-//   "Chole",
-//   "Paneer Butter Masala",
-//   "Shahi Paneer",
-//   "Palak Paneer",
-//   "Mixed Vegetables",
-//   "Bhindi Fry",
-//   "Aloo Gobi",
-//   "Matar Paneer",
-//   "Baingan Bharta",
-//   "Roti",
-//   "Naan",
-//   "Tandoori Roti",
-//   "Jeera Rice",
-//   "Curd",
-//   "Salad",
+  //   // 🍛 Lunch/Dinner Items
+  //   "Rice",
+  //   "Pulao",
+  //   "Biryani",
+  //   "Khichdi",
+  //   "Dal Tadka",
+  //   "Rajma",
+  //   "Chole",
+  //   "Paneer Butter Masala",
+  //   "Shahi Paneer",
+  //   "Palak Paneer",
+  //   "Mixed Vegetables",
+  //   "Bhindi Fry",
+  //   "Aloo Gobi",
+  //   "Matar Paneer",
+  //   "Baingan Bharta",
+  //   "Roti",
+  //   "Naan",
+  //   "Tandoori Roti",
+  //   "Jeera Rice",
+  //   "Curd",
+  //   "Salad",
 
-//   // 🍗 Non-Veg Items
-//   "Chicken Curry",
-//   "Chicken Biryani",
-//   "Butter Chicken",
-//   "Grilled Chicken",
-//   "Egg Curry",
-//   "Fish Curry",
-//   "Fried Fish",
-//   "Mutton Curry",
-//   "Kebab",
-//   "Prawns Fry",
+  //   // 🍗 Non-Veg Items
+  //   "Chicken Curry",
+  //   "Chicken Biryani",
+  //   "Butter Chicken",
+  //   "Grilled Chicken",
+  //   "Egg Curry",
+  //   "Fish Curry",
+  //   "Fried Fish",
+  //   "Mutton Curry",
+  //   "Kebab",
+  //   "Prawns Fry",
 
-//   // 🥗 Healthy Options
-//   "Sprouts",
-//   "Oats",
-//   "Brown Rice",
-//   "Quinoa",
-//   "Vegetable Soup",
-//   "Fruit Bowl",
-//   "Greek Yogurt",
-//   "Smoothie",
-//   "Boiled Vegetables",
+  //   // 🥗 Healthy Options
+  //   "Sprouts",
+  //   "Oats",
+  //   "Brown Rice",
+  //   "Quinoa",
+  //   "Vegetable Soup",
+  //   "Fruit Bowl",
+  //   "Greek Yogurt",
+  //   "Smoothie",
+  //   "Boiled Vegetables",
 
-//   // 🍞 Snacks
-//   "Samosa",
-//   "Pakora",
-//   "Sandwich",
-//   "Burger",
-//   "Pizza",
-//   "French Fries",
-//   "Chips",
-//   "Bhel Puri",
-//   "Pani Puri",
-//   "Sev Puri",
-//   "Pav Bhaji",
+  //   // 🍞 Snacks
+  //   "Samosa",
+  //   "Pakora",
+  //   "Sandwich",
+  //   "Burger",
+  //   "Pizza",
+  //   "French Fries",
+  //   "Chips",
+  //   "Bhel Puri",
+  //   "Pani Puri",
+  //   "Sev Puri",
+  //   "Pav Bhaji",
 
-//   // 🍰 Desserts
-//   "Ice Cream",
-//   "Gulab Jamun",
-//   "Rasgulla",
-//   "Kheer",
-//   "Halwa",
-//   "Brownie",
-//   "Cake",
-//   "Custard",
+  //   // 🍰 Desserts
+  //   "Ice Cream",
+  //   "Gulab Jamun",
+  //   "Rasgulla",
+  //   "Kheer",
+  //   "Halwa",
+  //   "Brownie",
+  //   "Cake",
+  //   "Custard",
 
-//   // 🧈 Other Essentials
-//   "Ghee",
-//   "Butter",
-//   "Cheese",
-//   "Yogurt",
-//   "Pickle",
-//   "Honey",
-//   "Salt",
-//   "Spices",
-// ];
+  //   // 🧈 Other Essentials
+  //   "Ghee",
+  //   "Butter",
+  //   "Cheese",
+  //   "Yogurt",
+  //   "Pickle",
+  //   "Honey",
+  //   "Salt",
+  //   "Spices",
+  // ];
 
   // let foodItems = []
 
-   const handleOnChange = (e) => {
+  const handleOnChange = (e) => {
     console.log(e.target.value);
+    textState(e.target.value)
   };
-
-
+  const myState = useState("Item search by user");
+  let textValue = myState[0];
+  let textState = myState[1];
 
   return (
     <>
-    <Container>
-      <h1 className="heading">Healthy Food</h1>
-      <ErrorMsg items={foodItems} />
-      <FoodInput handleOnChange={handleOnChange}/>
-      <AllItems items={foodItems} />
-    </Container>
+      <Container>
+        <h1 className="heading">Healthy Food</h1>
+        <ErrorMsg items={foodItems} />
+        <FoodInput handleOnChange={handleOnChange} />
+        <div style={{ padding: 10, textAlign: "center" }}>
+          {textValue}
+        </div>
+        <AllItems items={foodItems} />
+      </Container>
 
-    {/* <Container>
+      {/* <Container>
       <h1 className="heading">Healthy Foods more</h1>
         <ul className="list-group">
         {foodItems2.map((item) => (
