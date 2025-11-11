f9d000522708c6243f8c8ec292dce3ceb2062855
junaid1506 <junnu2757@gmail.com>
2025-11-11 14:36:52 +0530

    Update

--- Project/todo-app/src/Component/MainTodo.jsx
+++ Project/todo-app/src/Component/MainTodo.jsx
@@ -1,14 +1,14 @@
-function MainTodo({ todoName, todoDate ,handleDelete }) {
-  return (
-    <div className="row">
-      <div className="col-4">{todoName}</div>
-      <div className="col-4">{todoDate}</div>
-      <div className="col-2">
-        <button onClick={handleDelete} type="button" className="btn btn-danger">
-          Delete
-        </button>
+  function MainTodo({ todoName, todoDate ,handleDelete }) {
+    return (
+      <div className="row">
+        <div className="col-4">{todoName}</div>
+        <div className="col-4">{todoDate}</div>
+        <div className="col-2">
+          <button onClick={handleDelete} type="button" className="btn btn-danger">
+            Delete
+          </button>
+        </div>
       </div>
-    </div>
-  );
-}
-export default MainTodo;
+    );
+  }
+  export default MainTodo;
