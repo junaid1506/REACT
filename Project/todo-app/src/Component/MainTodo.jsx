f8152198aa9b0f9cf21e774a4c64aabf0132707d
junaid1506 <junnu2757@gmail.com>
2025-11-12 10:19:23 +0530

    Update

--- Project/todo-app/src/Component/MainTodo.jsx
+++ Project/todo-app/src/Component/MainTodo.jsx
@@ -1,17 +1,20 @@
-  import { MdDelete } from "react-icons/md";
-  
-  function MainTodo({ todoName, todoDate ,handleDelete }) {
-    return (
-      <div className="row">
-        <div className="col-4">{todoName}</div>
-        <div className="col-4">{todoDate}</div>
-        <div className="col-2">
-          <button onClick={()=>handleDelete(todoName)} type="button" className="btn btn-danger">
-          <MdDelete />
+import { MdDelete } from "react-icons/md";
 
-          </button>
-        </div>
+function MainTodo({ todoName, todoDate, handleDelete }) {
+  return (
+    <div className="row">
+      <div className="col-4">{todoName}</div>
+      <div className="col-4">{todoDate}</div>
+      <div className="col-2">
+        <button
+          onClick={() => handleDelete(todoName)}
+          type="button"
+          className="btn btn-danger"
+        >
+          <MdDelete />
+        </button>
       </div>
-    );
-  }
-  export default MainTodo;
+    </div>
+  );
+}
+export default MainTodo;
