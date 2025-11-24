b6ed485d498ab8f8aaa4ff31a925162fcc4d9649
junaid1506 <junnu2757@gmail.com>
2025-11-24 17:30:41 +0530

    Update

---
+++ Project/Social Media App Advance Vers/src/Component/LoadingSpiner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoadingSpiner = () => {
+  return (
+   <div className="my-spinner  spinner-border text-info" role="status">
+  <span className="visually-hidden">Loading...</span>
+</div>
+  )
+}
+
+export default LoadingSpiner
--- Project/Social Media App Advance Vers/src/Component/PostList.jsx
+++ Project/Social Media App Advance Vers/src/Component/PostList.jsx
@@ -2,20 +2,30 @@ import Post from "./Post";
 import { useContext, useEffect, useState } from "react";
 import { PostList as PostListContext } from "../Store/post-list-store";
 import EmptyMsg from "./EmptyMsg";
+import LoadingSpiner from "./LoadingSpiner";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListContext);
+  const [feteching, setFetching] = useState(false);
 
-  useEffect(()=>{
-     fetch("https://dummyjson.com/posts")
+  useEffect(() => {
+    console.log('fetch start ')
+    setFetching(true);
+    fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
-      .then((data) => addInitialPosts(data.posts));
-  },[])
+      .then((data) => {
+        addInitialPosts(data.posts);
+        setFetching(false);
+        console.log('fetch return ')
+      });
+      console.log('fetch end ')
+  }, []);
 
   return (
     <>
-      {postList.length === 0 && <EmptyMsg />}
-      {postList.map((item) => (
+    {feteching && <LoadingSpiner/>}
+      {!feteching && postList.length === 0 && <EmptyMsg />}
+      {!feteching && postList.map((item) => (
         <Post key={item.id} item={item} />
       ))}
     </>
