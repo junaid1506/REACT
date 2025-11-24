9cc13b101e368a0b75620826db11252ce914a659
junaid1506 <junnu2757@gmail.com>
2025-11-24 13:04:14 +0530

    Update

--- Project/Social Media App Advance Vers/src/Component/PostList.jsx
+++ Project/Social Media App Advance Vers/src/Component/PostList.jsx
@@ -1,18 +1,21 @@
 import Post from "./Post";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { PostList as PostListContext } from "../Store/post-list-store";
 import EmptyMsg from "./EmptyMsg";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListContext);
-  const [dataFetched, setDataFetched] = useState(false);
 
-  if (!dataFetched) {
-    fetch("https://dummyjson.com/posts")
+  useEffect(()=>{
+     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => addInitialPosts(data.posts));
-      setDataFetched(true)
-  }
+  },[])
+
+
+   
+   
+  
 
   return (
     <>
