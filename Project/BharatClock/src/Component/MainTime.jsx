24e344c29a52e19840f27206e46ed88fcb158e2c
junaid1506 <junnu2757@gmail.com>
2025-11-24 18:22:17 +0530

    Update

--- Project/BharatClock/src/Component/MainTime.jsx
+++ Project/BharatClock/src/Component/MainTime.jsx
@@ -1,10 +1,27 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 function MainTime(){
- const today = new Date();
- const dateOnly = today.toLocaleDateString();
- const timeOnly = today.toLocaleTimeString();
+ const [time,setTime] = useState(new Date());
+
+
+
+ useEffect(()=>{
+  const inetrValId =  setInterval(()=>{
+    setTime(new Date())
+  },1000)
+
+  return ()=>{
+    clearInterval(inetrValId)
+  }
+
+
+
+
+ },[])
 
   return(
-    <p>This is the current time: {dateOnly} - {timeOnly} </p>
+    <p>This is the current time: {time.toLocaleDateString()} - {time.toLocaleTimeString()} </p>
   )
 }
 
