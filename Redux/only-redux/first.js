1506eb5942bf250cc7e6dae0012128a95032650c
junaid1506 <junnu2757@gmail.com>
2025-11-28 18:28:06 +0530

    Update

--- Redux/only-redux/first.js
+++ Redux/only-redux/first.js
@@ -1,12 +1,29 @@
-const redux = require('redux')
-const reducer =  (store,action)=>{
-  return store; 
-}
-const store = redux.createStore(reducer)
+const redux = require("redux");
 
-const subcriber = ()=>{
-  const state =  store.getState()
-  console.log(state)
-}
+const INITIAL_VALUE = { counter: 0 };
+const reducer = (store = INITIAL_VALUE, action) => {
+  let newStore = store;
+  if (action.type === "INCRIMENT") {
+    newStore = { counter: store.counter + 1 };
+  } else if (action.type === "DECRIMENT") {
+    newStore = { counter: store.counter - 1 };
+  } else if (action.type === "ADDITION") {
+    newStore = { counter: store.counter + action.payload.number };
+  }
 
-store.subscribe(subcriber)
+  return newStore;
+};
+const store = redux.createStore(reducer);
+
+const subcriber = () => {
+  const state = store.getState();
+  console.log(state);
+};
+
+store.subscribe(subcriber);
+
+store.dispatch({ type: "INCRIMENT" });
+store.dispatch({ type: "DECRIMENT" });
+store.dispatch({ type: "INCRIMENT" });
+store.dispatch({ type: "ADDITION", payload: { number: 7 } });
+store.dispatch({ type: "DECRIMENT" });
