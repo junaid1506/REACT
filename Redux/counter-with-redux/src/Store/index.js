8e50293057327a37c862244a5c1e4cf4f6d13c1d
junaid1506 <junnu2757@gmail.com>
2025-11-29 18:13:04 +0530

    Update

--- Redux/counter-with-redux/src/Store/index.js
+++ Redux/counter-with-redux/src/Store/index.js
@@ -4,13 +4,12 @@ const INITIAL_VALUE = {
 };
 const counterReducer = (store = INITIAL_VALUE, action) => {
   if(action.type  === "INCRIMENT"){
-   store.counter =  store.counter + 1
+   return {counter  : store.counter + 1};
   }
   else if(action.type  === "DECREMENT"){
-   store.counter=  store.counter - 1
-  }
+  return {counter  : store.counter - 1}
+   }
   return store;
 };
-
 const counterStore = createStore(counterReducer);
 export default counterStore;
