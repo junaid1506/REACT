03ccdec7345d3522fd0455f7c69f63d55ed6c722
junaid1506 <junnu2757@gmail.com>
2025-11-29 11:19:15 +0530

    Update

--- Redux/only-redux/test.js
+++ Redux/only-redux/test.js
@@ -1,16 +1,20 @@
-const redux =  require('redux')
-const reducer = (store = 0 , action)=>{
-  let newStore =  store
-  if(action.type === 'INCRIMENT'){
-      newStore = store + 1
+const redux =  require("redux")
+const reducer = (store = 4 , action) =>{
+  newStore =  store 
+  if(action.type === "ADD"){
+    newStore =  store + action.payload.number
   }
-  return newStore 
+  return newStore
 }
+
+
 const store =  redux.createStore(reducer)
+
 const subcriber = ()=>{
   const state =  store.getState()
   console.log(state)
 }
 
 store.subscribe(subcriber)
-store.dispatch({type:'INCRIMENT'})
+
+store.dispatch({type:"ADD", payload:{number : 7}})
