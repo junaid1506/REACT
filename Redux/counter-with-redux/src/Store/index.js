5bb118232798faf80dabab5f16ff9262735e1238
junaid1506 <junnu2757@gmail.com>
2025-12-01 12:26:58 +0530

    Update

--- Redux/counter-with-redux/src/Store/index.js
+++ Redux/counter-with-redux/src/Store/index.js
@@ -5,19 +5,19 @@ const INITIAL_VALUE = {
 };
 const counterReducer = (store = INITIAL_VALUE, action) => {
   if(action.type  === "INCRIMENT"){
-   return {counter  : store.counter + 1 , privacy:store.privacy} ;
+   return {...store,  counter  : store.counter + 1 } ;
   }
   else if(action.type  === "DECREMENT"){
-  return {counter  : store.counter - 1 , privacy:store.privacy} 
+  return {...store, counter  : store.counter - 1 } 
    }
    else if(action.type === "ADD"){
-    return {counter : store.counter + action.payload.num , privacy:store.privacy}
+    return {...store, counter : store.counter + action.payload.num }
    }  
    else if(action.type === "SUB"){
-    return {counter : store.counter - action.payload.num , privacy:store.privacy}
+    return { ...store, counter : store.counter - action.payload.num }
    }
    else if(action.type === "HANDLEPRIVACY"){
-    return {privacy : !store.privacy , counter : store.counter}
+    return { ...store, privacy : !store.privacy }
    }
   return store;
 };
