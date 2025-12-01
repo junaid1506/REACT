ac95d172edce2ac0002e55f44f9ddbc75a2ea65c
junaid1506 <junnu2757@gmail.com>
2025-12-01 12:01:13 +0530

    Update

--- Redux/counter-with-redux/src/Component/Controls.jsx
+++ Redux/counter-with-redux/src/Component/Controls.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useDispatch } from "react-redux";
 
 const Controls = () => {
+  const inputElement = useRef();
   const dispatch = useDispatch();
   const handleIncriment = () => {
     dispatch({ type: "INCRIMENT" });
   };
   const handleDecriment = () => {
     dispatch({ type: "DECREMENT" });
+  };
+  const handleAdd = () => {
+    dispatch({
+      type: "ADD",
+      payload: {
+        num: Number(inputElement.current.value),
+      },
+    });
+    inputElement.current.value = "";
+  };
+  const handleSub = () => {
+    dispatch({
+      type: "SUB",
+      payload: {
+        num: Number(inputElement.current.value),
+      },
+    });
+    inputElement.current.value = "";
   };
   return (
     <>
@@ -20,10 +39,17 @@ const Controls = () => {
         </button>
       </div>
       <div className="d-grid gap-2 d-sm-flex justify-content-sm-center margin10">
-        <input type="text" placeholder="Enter number"  className="
-        "/>
-        <button onClick={handleDecriment} type="button" class="btn btn-info">
+        <input
+          type="text"
+          placeholder="Enter number"
+          className="inout-number"
+          ref={inputElement}
+        />
+        <button onClick={handleAdd} type="button" class="btn btn-info">
           Add
+        </button>
+        <button onClick={handleSub} type="button" class="btn btn-danger">
+          Sub
         </button>
       </div>
     </>
--- Redux/counter-with-redux/src/Store/index.js
+++ Redux/counter-with-redux/src/Store/index.js
@@ -9,6 +9,12 @@ const counterReducer = (store = INITIAL_VALUE, action) => {
   else if(action.type  === "DECREMENT"){
   return {counter  : store.counter - 1}
    }
+   else if(action.type === "ADD"){
+    return {counter : store.counter + action.payload.num}
+   }  
+   else if(action.type === "SUB"){
+    return {counter : store.counter - action.payload.num}
+   }
   return store;
 };
 const counterStore = createStore(counterReducer);
