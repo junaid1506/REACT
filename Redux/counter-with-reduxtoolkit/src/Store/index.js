439448873bb23ea464c01f72c85efcadcbc00b5f
junaid1506 <junnu2757@gmail.com>
2025-12-02 17:53:59 +0530

    Update

--- Redux/counter-with-reduxtoolkit/src/Store/index.js
+++ Redux/counter-with-reduxtoolkit/src/Store/index.js
@@ -10,24 +10,4 @@ const counterStore = configureStore({
 });
 
 export default counterStore;
-//   counter: 0,
-//   privacy : false
-// };
-// const counterReducer = (store = INITIAL_VALUE, action) => {
-//   if(action.type  === "INCRIMENT"){
-//    return {...store,  counter  : store.counter + 1 } ;
-//   }
-//   else if(action.type  === "DECREMENT"){
-//   return {...store, counter  : store.counter - 1 }
-//    }
-//    else if(action.type === "ADD"){
-//     return {...store, counter : store.counter + action.payload.num }
-//    }
-//    else if(action.type === "SUB"){
-//     return { ...store, counter : store.counter - action.payload.num }
-//    }
-//    else if(action.type === "HANDLEPRIVACY"){
-//     return { ...store, privacy : !store.privacy }
-//    }
-//   return store;
-// };
+
