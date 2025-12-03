bf0140cbe6e8945b8cb7c97a3e088f1c8ff1e6c3
junaid1506 <junnu2757@gmail.com>
2025-12-03 13:20:43 +0530

    Update

--- Project/Myntra Clone/3-myntra-react-clone/src/App.jsx
+++ Project/Myntra Clone/3-myntra-react-clone/src/App.jsx
@@ -1,12 +1,19 @@
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
+import HomeItem from "./Components/HomeItem";
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <div className="items-container"></div>
+        <div className="items-container">
+          <HomeItem />
+          <HomeItem />
+          <HomeItem />
+          <HomeItem />
+          <HomeItem />
+        </div>
       </main>
       <Footer />
     </>
