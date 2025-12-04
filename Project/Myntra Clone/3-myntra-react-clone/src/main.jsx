b1a4c774eb248b5d9b6522e26e40fe0360a5b43d
junaid1506 <junnu2757@gmail.com>
2025-12-04 17:30:39 +0530

    Update

--- Project/Myntra Clone/3-myntra-react-clone/src/main.jsx
+++ Project/Myntra Clone/3-myntra-react-clone/src/main.jsx
@@ -2,12 +2,29 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomeItemList from "./Components/HomeItemList.jsx";
+import Bag from "./Components/Bag.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomeItemList />,
+      },
+      {
+        path: "/bag",
+        element: <Bag />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>
 );
