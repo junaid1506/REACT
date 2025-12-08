468fbfe4feab328ab2b51b12af376856751a7427
junaid1506 <junnu2757@gmail.com>
2025-12-08 15:20:23 +0530

    Update

--- Project/Myntra Clone/3-myntra-react-clone/src/Components/FetchItems.jsx
+++ Project/Myntra Clone/3-myntra-react-clone/src/Components/FetchItems.jsx
@@ -1,8 +1,21 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const FetchItems = () => {
   const fetchStatus = useSelector((store) => store.fetchStatus);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (fetchStatus.fetchDone) return;
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    fetch("http://localhost:8080/items", { signal })
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch();
+      });
+  }, [fetchStatus]);
+
   return (
     <div>
       Fetch Done : {fetchStatus.fetchDone}
