23220910bab35461a90a4503c9e71a3c4df6bc06
junaid1506 <junnu2757@gmail.com>
2025-12-15 17:38:15 +0530

    Update

--- Project/Myntra Clone/3-myntra-react-clone/src/routes/Bag.jsx
+++ Project/Myntra Clone/3-myntra-react-clone/src/routes/Bag.jsx
@@ -1,8 +1,15 @@
+import { useSelector } from "react-redux";
 import BagItem from "../Components/BagItem";
 import BagSummary from "../Components/BagSummary";
 
 const Bag = () => {
-  
+  const bag = useSelector((store) => store.bag);
+  const items = useSelector((store) => store.items);
+  const item = bag
+    .map((id) => items.find((item) => item.id === id))
+    .filter(Boolean);
+  console.log(item);
+
   return (
     <>
       <main>
